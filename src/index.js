9a11276c3debddecc1b9aac0e53879d6e4853c76
Ponyo013 <163632247+Ponyo013@users.noreply.github.com>
2024-05-03 13:57:32 +0700

    Bang bantu

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import JawaBarat from "./component/Jawabarat";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <JawaBarat />
