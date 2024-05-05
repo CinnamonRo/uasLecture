f6784a2dfd2a007725463e0c98af163606001515
Cinamon <huangfredlie04@gmail.com>
2024-05-05 18:49:55 +0700

    update (masi bug cardnya)

--- src/component/Jawabarat.js
+++ src/component/Jawabarat.js
@@ -14,8 +14,8 @@ export default function App() {
   };
 
   return (
-    <div className="svg-container">
-      <div className="">
+    <div>
+      <div className="svg-container">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="900"
