7628b59fe047bca246df4da418aaa5396c57f309
Cinamon <huangfredlie04@gmail.com>
2024-05-06 14:26:47 +0700

    a

--- src/component/CardContent.js
+++ src/component/CardContent.js
@@ -1,13 +1,14 @@
 const imageContext = require.context("./FotoCard", false, /\.(png|jpe?g|svg)$/);
 
 const contents = [
-{
+  {
     title: "Suka Bumi",
-    content: "Kota Sukabumi, juga dikenal dengan sebutan Gunahuyi dalam bahasa lokalnya, adalah destinasi menarik di provinsi Jawa Barat, Indonesia. Kota ini memiliki daya tarik tersendiri bagi wisatawan yang mencari pengalaman yang autentik dan memikat di Jawa Barat.",
-    imageSrc:  imageContext("./JembatanGantung.jpeg"),
+    content:
+      "Kota Sukabumi, juga dikenal dengan sebutan Gunahuyi dalam bahasa lokalnya, adalah destinasi menarik di provinsi Jawa Barat, Indonesia. Kota ini memiliki daya tarik tersendiri bagi wisatawan yang mencari pengalaman yang autentik dan memikat di Jawa Barat.",
+    imageSrc: imageContext("./JembatanGantung.jpeg"),
     link: "SukaBumi.html",
-    buttonText: "Read More"
-}
-]
+    buttonText: "Read More",
+  },
+];
 
 export default contents;
--- src/component/navbar.js
+++ src/component/navbar.js
@@ -129,7 +129,41 @@ export default function Navbar() {
             </button>
           </div>
         </nav>
-        <Bar kabupatens={kabupatens} />
+        <div
+          class="top-28 max-w-full w-full z-[800] mx-2 hidden min-w-[180px] overflow-auto border border-blue-gray-50 bg-white p-2 font-sans text-sm font-normal text-blue-gray-500  shadow-lg shadow-blue-gray-500/10 focus:outline-none lg:block"
+          id=":r8:"
+          role="menu"
+          data-popover="menu"
+          data-popover-placement="bottom"
+        >
+          <ul
+            class="grid grid-cols-3 gap-y-2 outline-none outline-0"
+            role="menuitem"
+          >
+            {kabupatens.map((kabupaten, index) => (
+              <div key={index}>
+                <a href={kabupaten.link}>
+                  <button
+                    role="menuitem"
+                    class="flex w-full cursor-pointer select-none items-center gap-3 rounded-lg px-3 pb-2 pt-[9px] text-start leading-tight outline-none transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+                  >
+                    <div class="flex items-center justify-center rounded-lg !bg-blue-gray-50 p-2 ">
+                      <img src={kabupaten.img} alt={kabupaten.alt} />
+                    </div>
+                    <div>
+                      <h6 class="flex items-center font-sans text-sm font-bold tracking-normal text-blue-gray-900 antialiased">
+                        {kabupaten.title}
+                      </h6>
+                      <p class="block font-sans text-xs  !font-medium text-blue-gray-500 antialiased">
+                        {kabupaten.subtitle}
+                      </p>
+                    </div>
+                  </button>
+                </a>
+              </div>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
