3f83d20c4cb46381ac6f66408428928f81edfb7f
Ponyo013 <163632247+Ponyo013@users.noreply.github.com>
2024-05-08 08:43:41 +0700

    done footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import Navbar from "../component/navbar";
 import JawaBarat from "../component/Jawabarat";
-import FooterWithLogo from "../component/footer";
+import FooterWithLogo from "../component/footer"
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <JawaBarat />
+      <FooterWithLogo/>
     </div>
   );
 }
