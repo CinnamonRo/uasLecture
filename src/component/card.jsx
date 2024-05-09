db8fea7c2b398bec2493ae8db5c02a3ebe83d553
Cinamon <huangfredlie04@gmail.com>
2024-05-09 14:15:00 +0700

    Update card.jsx

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -3,36 +3,36 @@ import "../css/CardStyle.css";
 
 export default function Card(props) {
   return (
-    <div classame="Card">
-      <div classame="flex justify-center">
+    <div className="Card">
+      <div className="flex justify-center">
         <a
           href="/"
-          classame="rounded-3xl inline-block overflow-hidden shadow-xl max-w-xs cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-102 duration-300"
+          className="rounded-3xl inline-block overflow-hidden shadow-xl max-w-xs cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-102 duration-300"
         >
-          <div classame="relative group w-full overflow-hidden bg-black h-32 rounded-t-3xl">
+          <div className="relative group w-full overflow-hidden bg-black h-32 rounded-t-3xl">
             <img
               src={props.imageSrc}
               alt="kota"
-              classame="object-cover w-full h-full transform duration-700 backdrop-opacity-100"
+              className="object-cover w-full h-full transform duration-700 backdrop-opacity-100"
             />
-            <div classame="absolute bg-gradient-to-t from-black w-full h-full flex items-end justify-center -inset-y-0">
-              <h1 classame="font-bold text-2xl text-white mb-2">
+            <div className="absolute bg-gradient-to-t from-black w-full h-full flex items-end justify-center -inset-y-0">
+              <h1 className="font-bold text-2xl text-white mb-2">
                 {props.title}
               </h1>
             </div>
           </div>
-          <div classame="bg-white">
-            <div classame="text-center px-3 pb-6 pt-2">
-              <p classame="mt-2 font-sans font-light text-slate-700">
+          <div className="bg-white">
+            <div className="text-center px-3 pb-6 pt-2">
+              <p className="mt-2 font-sans font-light text-slate-700">
                 {props.content}
               </p>
             </div>
-            <div classame="flex justify-center pb-3 text-slate-700">
-              <div classame="text-center mr-3 border-r pr-3 last:border-r-0">
+            <div className="flex justify-center pb-3 text-slate-700">
+              <div className="text-center mr-3 border-r pr-3 last:border-r-0">
                 <h2>Population</h2>
                 <span>286k</span>
               </div>
-              <div classame="text-center mr-3 border-r pr-3 last:border-r-0">
+              <div className="text-center mr-3 border-r pr-3 last:border-r-0">
                 <h2>Area</h2>
                 <span>9.78 km²</span>
               </div>
