88cdb40fb56a5fa6dd0b8b5400d233d7237039d9
Cinamon <huangfredlie04@gmail.com>
2024-05-10 21:22:36 +0700

    Update client.jsx

--- src/component/client.jsx
+++ src/component/client.jsx
@@ -6,33 +6,28 @@ import { useState } from "react";
 const Client = () => {
   const [weatherInfo, setWeatherInfo] = useState(null);
 
+  const name = "";
+  const apiKey = "f21b4c899cccd9e952907f3ed4257ac0";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}`;
+  useEffect(() => {
+    const getWeatherInfo = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("error");
+        }
+        const data = await response.json();
+        setWeatherInfo(data);
+      } catch (Error) {
+        console.log("error");
+      }
+    };
+    getWeatherInfo();
+  }, [apiKey, url]);
 
-    const apiKey = "f21b4c899cccd9e952907f3ed4257ac0";
-    const url =`https://api.openweathermap.org/data/2.5/weather?lat=-6.905977&lon=107.613144&appid=${apiKey}`;
-    useEffect(()=>{ 
-       const getWeatherInfo = async()=>{
-         try {
-           const response = await fetch(url);
-           if (!response.ok) {
-             throw new Error("error");
-           }
-           const data = await response.json();
-           setWeatherInfo(data);
+  console.log(weatherInfo, "ini api");
 
-         } catch (Error) {
-           console.log("error");
-         }
-       }
-        getWeatherInfo();
-    },[apiKey, url]);
-
-    console.log(weatherInfo, "ini api");
-
-  return <div>
-{weatherInfo && (
-<p>{weatherInfo.base}</p>
-)}
-  </div>;
+  return <div>{weatherInfo && <p>{weatherInfo.base}</p>}</div>;
 };
 
 export default Client;
