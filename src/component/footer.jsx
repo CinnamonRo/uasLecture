46b778a29205e0481f6cb03942ee2944b6c31467
Cinamon <huangfredlie04@gmail.com>
2024-05-13 16:03:27 +0700

    footer

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -7,11 +7,11 @@ export default function Footer() {
         <div class="px-4 pt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
           <div class="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
             <div class="md:max-w-md lg:col-span-2">
-              <div className="w-20 h-20">
+              <div className="w-20 h-20 ">
                 <img src="images/othersLogo/umn.png" alt="" />
               </div>
-              <span class="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
-                Informatika 2023<br></br>- Pre Intro To Tecnology
+              <span class="text-xl font-bold tracking-wide text-gray-100 uppercase text-left">
+                Informatika 2023<br></br> Pre Intro To Tecnology
               </span>
             </div>
             <div class="grid grid-cols-2 row-gap-8 lg:col-span-4 md:grid-cols-3">
@@ -60,11 +60,11 @@ export default function Footer() {
                   <li className="flex pb-2">
                     <img
                       className="h-6 w-12"
-                      src="/images/othersLogo/openweatherLogo.png"
+                      src="/images/othersLogo/weatherApi.png"
                       alt="Open Weather"
                     />
-                    <a className="pb-2 pl-2" href="https://openweathermap.org/">
-                      Open Weather
+                    <a className="pb-2 pl-2" href="https://www.weatherapi.com/">
+                      Weather api
                     </a>
                   </li>
                 </ul>
@@ -77,8 +77,8 @@ export default function Footer() {
                   <li className="flex pb-2">
                     <img
                       className="h-6 w-18"
-                      src="/images/othersLogo/DaisyUi.png"
-                      alt="Open Weather"
+                      src="/images/svg/daisyuilogo.svg"
+                      alt="Daisy Ui"
                     />
                     <a className="pb-2 pl-2" href="https://daisyui.com/">
                       Daisy UI
@@ -88,7 +88,7 @@ export default function Footer() {
                     <img
                       className="h-6 w-6"
                       src="/images/othersLogo/Materialwind.png"
-                      alt="Open Weather"
+                      alt="Material Tailwind"
                     />
                     <a
                       className="pb-2 pl-2"
