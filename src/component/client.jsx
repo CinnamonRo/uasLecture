85179c055759b49af9f0f9c9728c5e8538ca2e97
Cinamon <huangfredlie04@gmail.com>
2024-05-14 15:20:36 +0700

    Update client.jsx

--- src/component/client.jsx
+++ src/component/client.jsx
@@ -7,8 +7,8 @@ import Card from "./card";
 const Client = (props) => {
   const [apiInfo, setApiInfo] = useState(null);
   //   const cityName = '';
-  const apiKey = "f21b4c899cccd9e952907f3ed4257ac0";
-  const url = `http://api.weatherapi.com/v1/current.json?key=3b52f5ead5cd41a99fc23500241305&q=${props.cityName}&aqi=no`;
+  const apiKey = "d721d22fabe0487caba74553241405";
+  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${props.cityName}&aqi=no`;
   useEffect(() => {
     const getApiInfo = async () => {
       try {
