dd22e7abfd8f4bea2c285146d89f2c893be0e05f
Ponyo013 <163632247+Ponyo013@users.noreply.github.com>
2024-05-14 13:37:53 +0700

    .

--- src/component/searchBox.jsx
+++ src/component/searchBox.jsx
@@ -14,16 +14,18 @@ export default function Search() {
         >
           <span className="[&>svg]:h-5 [&>svg]:w-5">
             <svg
+              class="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="black"
+              viewBox="0 0 20 20"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
               />
             </svg>
           </span>
@@ -39,14 +41,46 @@ export default function Search() {
           </span>
         </button>
         {isOpen && (
-          <div>
-            <input
-              className="py-2 rounded text-center"
-              type="search"
-              placeholder="Enter City (Ex: Bandung)"
-              style={{ outline: "none", fontFamily: "Arial, sans-serif" }}
-            />
-          </div>
+          <form class="max-w-md mx-auto">
+            <label
+              for="default-search"
+              class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div class="relative">
+              <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"></div>
+              <input
+                type="search"
+                id="default-search"
+                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter a city"
+                required
+              />
+              <button
+                onClick={() =>
+                  document.getElementById("my_modal_3").showModal()
+                }
+                type="submit"
+                class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+              <dialog id="my_modal_3" className="modal">
+                <div className="modal-box">
+                  <form method="dialog">
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                      ✕
+                    </button>
+                  </form>
+                  <h3 className="font-bold text-lg">Hello!</h3>
+                  <p className="py-4">
+                    Press ESC key or click on ✕ button to close
+                  </p>
+                </div>
+              </dialog>
+            </div>
+          </form>
         )}
       </div>
     </div>
