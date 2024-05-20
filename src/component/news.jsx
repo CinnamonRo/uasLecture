900d1da9c425998c4e51b8665dae510c4334156f
Cinamon <huangfredlie04@gmail.com>
2024-05-20 18:38:49 +0700

    .

--- src/component/news.jsx
+++ src/component/news.jsx
@@ -4,7 +4,7 @@ function News() {
   const [apiInfo, setApiInfo] = useState(null);
   const [loading, setLoading] = useState(true); // Added loading state
   //   const cityName = '';
-  const apiKey = "c6e6b946b6294590ab2e4f011068653c";
+  const apiKey = "95622e90f2b84ece81b0d5057b3e75ff";
   const url = `https://newsapi.org/v2/top-headlines?q=city&apiKey=${apiKey}`;
   useEffect(() => {
     const getApiInfo = async () => {
